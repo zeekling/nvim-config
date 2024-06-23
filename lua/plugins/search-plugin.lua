06a604614bfef349081bd72e0c82bb57d56eb973
zeekling <lingzhaohui@zeekling.cn>
2024-06-23 13:29:45 +0800

    优化搜索界面显示

--- lua/plugins/search-plugin.lua
+++ lua/plugins/search-plugin.lua
@@ -3,5 +3,18 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					layout_strategy = "vertical",
+					layout_config = {
+						vertical = { width = 0.8 },
+					},
+					pickers = {
+						theme = "dropdown",
+					},
+				},
+			})
+		end,
 	},
 }
