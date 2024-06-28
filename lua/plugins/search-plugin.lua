d31c683823b7c89668e28f9d9f93c8c3555bd648
zeekling <lingzhaohui@zeekling.cn>
2024-06-28 22:33:49 +0800

    优化搜索

--- lua/plugins/search-plugin.lua
+++ lua/plugins/search-plugin.lua
@@ -3,7 +3,18 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
+			{
+				"nvim-lua/plenary.nvim",
+				{
+					"nvim-telescope/telescope-fzf-native.nvim",
+					build = "make",
+					cond = function()
+						return vim.fn.executable("make") == 1
+					end,
+				},
+				"nvim-telescope/telescope-ui-select.nvim",
+				"nvim-tree/nvim-web-devicons",
+			},
 		},
 	},
 	{
@@ -30,9 +41,14 @@ return {
 					live_grep_args = {
 						auto_quoting = true,
 					},
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
 				},
 			})
 			telescope.load_extension("live_grep_args")
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
 		end,
 	},
 }
